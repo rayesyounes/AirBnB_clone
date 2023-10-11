c891e2a8404eadc4dcffbc19a31761f40b5f4eeb
SihamEl23 <elghazi.siham1101@gmail.com>
2023-10-11 14:21:25 -0700

    import more classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,6 +39,11 @@ class FileStorage:
 		"""
 		from models.base_model import BaseModel
 		from models.user import User
+                from models.amenity import Amenity
+                from models.state import State
+                from models.city import City
+                from models.place import Place
+                from models.review import Review
 		class_mapping = {
 			"BaseModel": BaseModel,
 			"User": User,
