8ded64778a7ebe85e5839c6347fbbeeea406d043
SihamEl23 <elghazi.siham1101@gmail.com>
2023-10-12 04:59:19 -0700

    Fix some import moduls issues

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 """
 Create a unique FileStorage instance
 """
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
+
--- test_base_model_dict.py
+++ test_base_model_dict.py
@@ -22,6 +22,4 @@ print(type(my_new_model.created_at))
 
 print("--")
 print(my_model is my_new_model)
-print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                   my_model_json[key]))
 
